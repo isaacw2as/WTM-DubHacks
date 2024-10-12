b18d8e5004f8c9d0238dce4cfdb45077aa5aa249
PinguinZero <39572177+olegian@users.noreply.github.com>
2024-10-12 14:58:47 -0700

    inital commit

---
+++ backend/db.py
@@ -0,0 +1,14 @@
+import pymongo
+import uuid
+import os
+
+uri = f"mongodb+srv://olegianch:{}@wtm-cluster.bl2wm.mongodb.net/?retryWrites=true&w=majority&appName=wtm-cluster"
+
+def register_new_user(uuid: str, username: str, password: str, interest_ids: list):
+  pymongo
+
+  pass
+
+
+if __name__ == "__main__":
+  register_new_user()
