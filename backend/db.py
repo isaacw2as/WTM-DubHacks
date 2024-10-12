90d377801aac5b09417bb807ace534dc4d41fc17
PinguinZero <39572177+olegian@users.noreply.github.com>
2024-10-12 16:15:18 -0700

    db event management

--- backend/db.py
+++ backend/db.py
@@ -1,7 +1,9 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 import os
+import logging
 
+logger = logging.getLogger(__name__)
 class DatabaseClient:
   def __init__(self):
     DB_PASS = os.environ.get("DB_PASSWORD", "WTMPassword")
@@ -16,7 +18,7 @@ class DatabaseClient:
       return res != None
 
     except Exception as e:
-      print(e)
+      logger.error(f"DB: User exists error: {e}")
 
     return False
 
@@ -34,22 +36,24 @@ class DatabaseClient:
           "latestEid": 0,
         }
       )
-      print(res)
+      logger.info(f"DB: registered new user: {username}")
       
     except Exception as e:
-      print(e)
+      logger.error(f"DB: User register: {e}")
 
   def login(self, username, password):
     try:
       users_collection = self.db.get_collection("USERS")
       res = users_collection.find_one({"username": username})
       if res == None:
+        logger.warning(f"DB: login with non existant user: {username}")
         return False
       
+      logger.info(f"DB: logged in: {username}")
       return res['password'] == password
       
     except Exception as e:
-      print(e)
+      logger.error(f"DB: Login error: {e}")
     
     return False
 
@@ -58,28 +62,77 @@ class DatabaseClient:
       users_collection = self.db.get_collection("USERS")
       res = users_collection.find_one({"username": my_username})
       if res == None:
+        logger.warning(f"DB: is friend of nonexistant user: {my_username}")
         return False
 
       return friends_username in res['friends']
     except Exception as e:
-      print(e)
+      logger.error(f"DB: is friend error: {e}")
     
     return False
-
 
   def add_friend(self, my_username, friends_username):
     try:
       users_collection = self.db.get_collection("USERS")
       res = users_collection.find_one_and_update({ "username": my_username }, {"$push": {"friends": friends_username}})
 
+      logger.info(f"DB: add friend {friends_username=} of user {my_username=}")
       return res != None
 
     except Exception as e:
-      print(e)
+      logger.error(f"DB: Add friend error: {my_username}")
     
     return False
+
+  def is_pending_event(self, username, event_id):
+    try:
+      users_collection = self.db.get_collection("USERS")
+      res = users_collection.find_one({ "username": username })
+      if res == None:
+        logger.warning(f"DB: checking pending event for non existant user {username}")
+        return False
+      
+      return event_id in res['pendingEids']
+      
+    except Exception as e:
+      logger.error(f"DB: is pending event error: {e}")
+      
+    return False
+
+  def add_pending_event(self, username, event_id):
+    try:
+      users_collection = self.db.get_collection("USERS")
+      res = users_collection.find_one_and_update({ "username": username }, {"$push": {"pendingEids": event_id}})
+      logger.info(f"DB: added event {event_id} to user pending events of {username}")
+
+      return res != None
+
+    except Exception as e:
+      logger.error(f"DB: Add pending event error: {username=}, {event_id}=")
+    
+    return False
+
+  def move_pending_event(self, username, event_id):
+    try:
+      users_collection = self.db.get_collection("USERS")
+      res = users_collection.find_one_and_update({ "username": username }, {"$pull": {"pendingEids": event_id}})
+      if res == None:
+        logger.warning(f"DB: Not supposed to happen1")
+        return False
+
+      res = users_collection.find_one_and_update({"username": username}, {"$push": {"pastEids": event_id}})
+      if res == None:
+        logger.warning(f"DB: Not supposed to happen2")
+        return False
+
+      return True
+    except Exception as e:
+      logger.error(f"DB: move pending event error: {e}")
+    
+    return False
+    
     
 
 if __name__ == "__main__":
   d = DatabaseClient()
-  print(d.is_friend("username0", "username1"))
+  print(d.move_pending_event("username0", "event0"))
