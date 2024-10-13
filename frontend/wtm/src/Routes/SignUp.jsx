57181b0a6e463f82781252f38098f4bd894add2e
carl-searle <carl1298@outlook.com>
2024-10-13 05:19:03 -0700

    combined sign up page

--- frontend/wtm/src/Routes/SignUp.jsx
+++ frontend/wtm/src/Routes/SignUp.jsx
@@ -6,21 +6,39 @@ export default function SignUp({ }) {
   return (
     <>
       <div>
-        <div className='signuphalf'>
-          <div className='header'>Sign Up</div>
+        <div className='signupscreen'>
+          <div className='signUpHeader'>Sign Up</div>
           <div>
             <input type='text' className='signUpInput' name='username' placeholder='Username' maxLength={24}/>
           </div>
           <div>
             <input type='password' className='signUpInput' name='password' placeholder='Password' maxLength={40}/>
           </div>
-        </div>
-        <div className='signuphalf'>
+          <div>
+              <p>Select up to 3 types of events you're interested in attending.</p>
+          </div>
+          <div className='buttonGrid'>
+            <div>
+                <button className='interestbutton'>Art</button>
+                <button className='interestbutton'>Music</button>
+                <button className='interestbutton'>Gaming</button>
+            </div>
+            <div>
+                <button className='interestbutton'>Nature</button>
+                <button className='interestbutton'>Culture</button>
+                <button className='interestbutton'>Sports</button>
+            </div>
+            <div>
+                <button className='interestbutton'>Fitness</button>
+                <button className='interestbutton'>Travel</button>
+                <button className='interestbutton'>Food</button>
+            </div>
+          </div>
           <button className='createAccount'>Create An Account</button>
           <div>Have an Account? <Link to='/' className='login'>Log In</Link></div>
         </div>
+        {/* make button change color when clicked (button:click)*/}
       </div>
     </>
-    // make button change color when clicked (button:click)
   )
 }
