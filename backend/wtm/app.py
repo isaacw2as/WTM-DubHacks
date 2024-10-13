544d25d7d3a18ccb9b65db10819ba02f8a8223d7
Isaac Yun <isaac.yun@spectrumeffect.com>
2024-10-12 21:36:10 -0700

    Reformatted app.py - combined it with wtm - and updated launch.json to run app.py

--- backend/wtm/app.py
+++ backend/wtm/app.py
@@ -1,8 +1,12 @@
 from flask import Flask
+from gevent.pywsgi import WSGIServer
 from backend.wtm.routes.users import users
 from backend.wtm.routes.login import login
 from backend.wtm.routes.friends import friends
 from backend.wtm.routes.events import events
+
+URL = "0.0.0.0"
+PORT = 42069
 
 def register_routes(app, routes):
     for route in routes:
@@ -15,4 +19,8 @@ def create_app():
 
     return app
 
+if __name__ == "__main__":
+    app = create_app()
+    http_server = WSGIServer((URL, PORT), app.wsgi_app)
+    http_server.serve_forever()
 
