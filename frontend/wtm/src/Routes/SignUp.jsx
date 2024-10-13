622cf42c75ee4b72ce69db42b060c8a85c455d87
carl-searle <carl1298@outlook.com>
2024-10-12 23:23:10 -0700

    front-end sign up page

--- frontend/wtm/src/Routes/SignUp.jsx
+++ frontend/wtm/src/Routes/SignUp.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+//<Link to ={user.id}>{user.name}</Link>
 import '../SignUp.css';
 
 export default function SignUp({ }) {
   return (
     <>
       <div className='background'>
-        <div className='header'>Sign Up</div>
-        <div>LoginInfo</div>
-        <div>Create Account</div>
-        <div>Have an Account? Log In</div>
+        <div className='half'>
+          <div className='header'>Sign Up</div>
+          <div>
+            <input type='text' name='username' placeholder='Username' maxLength={24}/>
+          </div>
+          <div>
+            <input type='passsword' name='password' placeholder='Password' maxLength={40}/>
+          </div>
+        </div>
+        <div className='half'>
+          <button className='createAccount'>Create An Account</button>
+          <div>Have an Account? <span className='login'>Log In</span></div>
+        </div>
       </div>
     </>
+    // make button change color when clicked (button:click)
   )
 }
