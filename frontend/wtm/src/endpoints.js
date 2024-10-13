0bbba4e08b6429f724d684cfc4a69726cf4004f6
Oleg Ianchenko <oleg.ianch@gmail.com>
2024-10-13 00:59:05 -0700

    endpoints centralized

---
+++ frontend/wtm/src/endpoints.js
@@ -0,0 +1,5 @@
+const BASE_URL = "http://localhost:42069"
+
+export const UPLOAD_ENDPOINT   = BASE_URL + "/upload_files/upload"
+export const DOWNLOAD_ENDPOINT = BASE_URL + "/upload_files/download"
+export const GET_FEED_ENDPOINT = BASE_URL + "/feed/get"
